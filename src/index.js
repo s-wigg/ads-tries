f47a9e4990ba09b6829be15c0148b8e543ca8a5d
Dan Roberts <dan@adadevelopersacademy.org>
2020-03-24 15:37:54 -0700

    More RXJS stuff

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import keyboardButtons from './keyboard_buttons';
 import * as rxjs from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map, scan } from 'rxjs/operators'
 
 const buildKeyboard = (keyboardContainer, buttonTemplate) => {
-  const observables = [];
-  keyboardButtons.forEach(button => {
-    const buttonElem = buttonTemplate.content.cloneNode(true);
+  const observables = keyboardButtons.map(button => {
+    const buttonFragment = buttonTemplate.content.cloneNode(true);
+    const element = buttonFragment.firstElementChild;
 
-    buttonElem.querySelector('.keyboard-button--number').textContent = button.number;
-    buttonElem.querySelector('.keyboard-button--subtext').textContent = button.subtext;
+    element.querySelector('.keyboard-button--number').textContent = button.number;
+    element.querySelector('.keyboard-button--subtext').textContent = button.subtext;
 
-    observables.push(
-      rxjs.fromEvent(buttonElem.firstElementChild, 'click')
-        .pipe(map(() => button.number)) //.subscribe((x) => console.log(`click ${x}`))
-    );
+    keyboardContainer.appendChild(element);
 
-    keyboardContainer.appendChild(buttonElem);
+    return rxjs.fromEvent(element, 'click').pipe(map(() => button.number))
   });
 
   return rxjs.merge(...observables);
@@ -24,15 +21,19 @@ const buildKeyboard = (keyboardContainer, buttonTemplate) => {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+
   const keyboard = document.querySelector('.keyboard');
   const buttonTemplate = document.querySelector('.template--keyboard-button');
-  
+
   const keyPressObservable = buildKeyboard(keyboard, buttonTemplate);
-  
+
   const numberDisplay = document.querySelector('.display--numbers');
-  keyPressObservable.subscribe(number => {
-    console.log(`${number} key pressed`);
-    numberDisplay.value += number;
-  })
+  keyPressObservable.pipe(
+    scan((value, key) => {
+      if (key === '#') {
+        return value.slice(0, -1);
+      }
+      return value + key;
+    }, ''),
+  ).subscribe(value => numberDisplay.value = value);
 });
