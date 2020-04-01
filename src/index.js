fb48d500fdf9d2520d4387dd3f5a190fd8b691eb
Dan Roberts <dan@adadevelopersacademy.org>
2020-04-01 10:39:35 -0700

    Remember to give the display a datastructure to work with

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import words10KRaw from '../10000-words.txt';
+const words10K = words10KRaw.split('\n');
+
 import WordList from './data_structures/word_list';
 
 import PerformanceReport from './components/PerformanceReport';
@@ -7,8 +10,7 @@ import Display from './components/Display';
 document.addEventListener("DOMContentLoaded", () => {
   const keyboardElement = document.querySelector('.keyboard');
   const keyboard = new Keyboard(keyboardElement);
-
-  const display = new Display(keyboard.keyPressObservable);
+  const display = new Display(keyboard.keyPressObservable, new WordList(words10K));
   
   // console.log(profile(WordList));
   const perfReport = new PerformanceReport(WordList);
