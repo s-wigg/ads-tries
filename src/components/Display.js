cc5ea32ae1ce411c24ae3180b07cae46c2c031c2
Dan Roberts <dan@adadevelopersacademy.org>
2020-04-01 07:49:48 -0700

    Break display out as a separate component

---
+++ src/components/Display.js
@@ -0,0 +1,27 @@
+import { scan } from 'rxjs/operators';
+
+class Display {
+  constructor(keyPressObservable, dataStructure) {
+    this.dataStructure = dataStructure;
+    
+    const keyCodeStream = keyPressObservable.pipe(
+      scan((value, key) => {
+        if (key === '#') {
+          return value.slice(0, -1);
+        }
+        return value + key;
+      }, ''),
+    );
+  
+    const numberDisplay = document.querySelector('.display--numbers');
+    keyCodeStream.subscribe(value => numberDisplay.value = value);
+  
+    const wordsDisplay = document.querySelector('.display--words');
+    keyCodeStream.subscribe(value => {
+      const possibleWords = this.dataStructure.lookupPrefix(value);
+      wordsDisplay.value = possibleWords.slice(0, 5).join(', ');
+    });
+  }
+}
+
+export default Display;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 
-import { scan } from 'rxjs/operators';
+
 
 import words10KRaw from '../10000-words.txt';
 const words10K = words10KRaw.split('\n');
@@ -11,38 +11,12 @@ import profile from './performance';
 
 import PerformanceReport from './components/PerformanceReport';
 import Keyboard from './components/Keyboard';
-
-
-const buildDisplay = (keyPressObservable) => {
-  const keyCodeStream = keyPressObservable.pipe(
-    scan((value, key) => {
-      if (key === '#') {
-        return value.slice(0, -1);
-      }
-      return value + key;
-    }, ''),
-  );
-
-  const numberDisplay = document.querySelector('.display--numbers');
-  keyCodeStream.subscribe(value => numberDisplay.value = value);
-
-  const dataStructures = {
-    wordList: new WordList(words10K),
-  };
-  let selectedDataStructure = dataStructures.wordList;
-
-  const wordsDisplay = document.querySelector('.display--words');
-  keyCodeStream.subscribe(value => {
-    const possibleWords = selectedDataStructure.lookupPrefix(value);
-    wordsDisplay.value = possibleWords.slice(0, 5).join(', ');
-  });
-}
+import Display from './components/Display';
 
 document.addEventListener("DOMContentLoaded", () => {
   const keyboardElement = document.querySelector('.keyboard');
   const keyboard = new Keyboard(keyboardElement);
-
-  buildDisplay(keyboard.keyPressObservable);
+  const display = new Display(keyboard.keyPressObservable);
   
   // console.log(profile(WordList));
   const perfReport = new PerformanceReport(WordList);
